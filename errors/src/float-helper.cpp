2c6b960ce6701956dc23cbeaa9467ff480e2f5fe
Sergii Puzyrov <sergii.puzyrov@gmail.com>
2026-09-06 20:13:50 +0300

    Добавьте файлы проекта.

---
+++ errors/src/float-helper.cpp
@@ -0,0 +1,32 @@
+﻿#include "float-helper.h"
+
+namespace math_helpers {
+	const double FloatHelper::round_to_sig(const int& digits) const
+	{
+		using std::fabs;
+		using std::floor;
+		using std::log10;
+		using std::pow;
+		using std::round;
+		
+		if (source == 0.0)
+		{
+			return 0.0;
+		}
+
+		auto abs_value = fabs(this->source);
+
+		auto exponent = static_cast<int>(floor(log10(abs_value) + 1e-12));
+
+		auto factor = pow(10.0, digits - 1 - exponent);
+
+		return round(this->source * factor) / factor;
+	}
+
+	double round_to_signum_digits(const double& source, const int& sign_digits)
+	{
+		auto helper = FloatHelper(source);
+
+		return helper.round_to_sig(sign_digits);
+	}
+}
---
+++ errors/src/main.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <iomanip>
+#include "float-helper.h"
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::setprecision;
+
+void process_rounding_to(const char* msg, const double&, const int&);
+
+int main()
+{
+	auto big_number = 123456789.012378253;
+	
+	auto small_number = 0.000289898457879823992;
+
+	int sign_digits;
+
+	cout << "Enter signum digits:";
+	cin >> sign_digits;
+
+	process_rounding_to("Big number: ", big_number, sign_digits);
+
+	process_rounding_to("Small number: ", small_number, sign_digits);
+
+	return 0;
+}
+
+void process_rounding_to(const char* msg, const double& source, const int& digits)
+{
+	using math_helpers::round_to_signum_digits;
+
+	auto result = round_to_signum_digits(source, digits);
+
+	cout << setprecision(12);
+
+	cout << msg << result << endl;
+}
